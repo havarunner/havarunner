22d3c947205a7f82c00a1ba4ba50943dd8a8867a
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-09-10 07:29:51 +0300

    Remove dead code

--- src/main/scala/havarunner/HavaRunnerHelper.scala
+++
@@ -1,9 +0,0 @@
-package havarunner
-
-import org.junit.runners.model.{FrameworkMethod, TestClass}
-import java.lang.reflect.Method
-import org.junit._
-import havarunner.ScenarioHelper._
-import scala.collection.JavaConversions._
-import java.lang.annotation.Annotation
-
