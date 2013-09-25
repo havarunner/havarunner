3f23524902515bbc2241ce201a465f85e95d89d5
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-09-25 12:25:02 +0300

    Make AbstractSuiteMember actually abstract

--- src/test/java/com/github/havarunner/suite/suitedefinedinparent/AbstractSuiteMember.java
+++ src/test/java/com/github/havarunner/suite/suitedefinedinparent/AbstractSuiteMember.java
@@ -3,5 +3,5 @@ package com.github.havarunner.suite.suitedefinedinparent;
 import com.github.havarunner.annotation.PartOf;
 
 @PartOf(Suite.class)
-class AbstractSuiteMember {
+abstract class AbstractSuiteMember {
 }
