15f7e67e4b3b6f461e0adbf9d89f34b74c3c7bde
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-10-06 14:28:20 +0300

    Test that each @Rule is applied on each test

--- src/test/java/com/github/havarunner/rules/RuleTest.java
+++ src/test/java/com/github/havarunner/rules/RuleTest.java
@@ -1,28 +1,47 @@
 package com.github.havarunner.rules;
 
 import com.github.havarunner.HavaRunner;
+import com.google.common.collect.Lists;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+import java.util.Collection;
+import java.util.List;
+
 import static com.github.havarunner.TestHelper.run;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RuleTest {
-
-    static Description firstRuleDescription;
-    static Description secondRuleDescription;
-    static boolean testRun;
+    static Collection<Description> firstRuleApplications= Lists.newArrayList();
+    static Collection<Description> secondRuleApplications = Lists.newArrayList();
+    static boolean firstTestRun;
+    static boolean secondTestRun;
 
     @Test
     public void HavaRunner_supports_JUnit_rules() {
         run(new HavaRunner(TestWithTwoRules.class));
-        assertEquals("hello", firstRuleDescription.getMethodName());
-        assertEquals("hello", secondRuleDescription.getMethodName());
-        assertTrue(testRun);
+        verify(firstRuleApplications);
+        verify(secondRuleApplications);
+        assertTrue(firstTestRun);
+        assertTrue(secondTestRun);
+    }
+
+    private void verify(Collection<Description> ruleApplications) {
+        assertEquals(2, ruleApplications.size());
+        HavaRunner_applies_each_rule_on_each_test_method(ruleApplications);
+    }
+
+    private void HavaRunner_applies_each_rule_on_each_test_method(Collection<Description> ruleApplications) {
+        List<String> calledTestMethods = Lists.newArrayList();
+        for (Description ruleApplication : ruleApplications) {
+            calledTestMethods.add(ruleApplication.getMethodName());
+        }
+        assertTrue(calledTestMethods.contains("hello"));
+        assertTrue(calledTestMethods.contains("helloAgain"));
     }
 
     static class TestWithTwoRules {
@@ -30,7 +49,7 @@ public class RuleTest {
         TestRule first = new TestRule() {
             @Override
             public Statement apply(Statement base, Description description) {
-                firstRuleDescription = description;
+                firstRuleApplications.add(description);
                 return base;
             }
         };
@@ -39,14 +58,19 @@ public class RuleTest {
         TestRule second = new TestRule() {
             @Override
             public Statement apply(Statement base, Description description) {
-                secondRuleDescription = description;
+                secondRuleApplications.add(description);
                 return base;
             }
         };
 
         @Test
         void hello() {
-            testRun = true;
+            firstTestRun = true;
+        }
+
+        @Test
+        void helloAgain() {
+            secondTestRun = true;
         }
     }
 }
