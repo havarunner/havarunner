c53583c2d6f70bb53beeb8108217039c94ea42c0
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-10-28 13:43:41 +0200

    Add empty parentheses

--- src/main/scala/com/github/havarunner/HavaRunner.scala
+++ src/main/scala/com/github/havarunner/HavaRunner.scala
@@ -57,12 +57,11 @@ class HavaRunner(parentClass: Class[_ <: Any]) extends Runner with Filterable wi
     this.filterOption = Some(filter)
   }
 
-  private[havarunner] def reportIfSuite = {
+  private[havarunner] def reportIfSuite() =
     children.filter(_.testContext.isInstanceOf[SuiteContext]).foreach(testAndParameters => {
       val suiteContext: SuiteContext = testAndParameters.testContext.asInstanceOf[SuiteContext]
       println(s"[HavaRunner] Running ${testAndParameters.testClass.getName} as a part of ${suiteContext.suiteClass.getName}")
     })
-  }
 
   private[havarunner] def runChild(implicit testAndParameters: TestAndParameters, notifier: RunNotifier): Option[FutureTask[_]] = {
     implicit val description = describeChild
