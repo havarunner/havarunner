facdf00b2b0ac7be2ad62b70a1812fa08fea5b70
Tomi Takussaari <tomi.takussaari@elisa.fi>
2014-08-28 12:39:02 +0300

    Add test cases for verifying that HavaRunner runs tests multiple times when:
    - Test is member of suite
    - Test has @AfterAll annotation

---
+++ src/test/java/com/github/havarunner/scenarios/duplicated/VerifyThatTestAreRunOnlyOnceTestSuperClass.java
@@ -0,0 +1,33 @@
+package com.github.havarunner.scenarios.duplicated;
+
+import com.github.havarunner.annotation.AfterAll;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertTrue;
+
+abstract class VerifyThatTestAreRunOnlyOnceTestSuperClass {
+    protected final AtomicInteger suiteObject;
+    protected final List<String> scenarios;
+    VerifyThatTestAreRunOnlyOnceTestSuperClass(AtomicInteger suiteObject, List<String> scenarios) {
+        this.suiteObject = suiteObject;
+        this.scenarios = scenarios;
+    }
+    @Test
+    public void verifyTestIsRanOnlyOncePerScenario() {
+        final int runNumber = suiteObject.addAndGet(1);
+        System.out.println("Run: "+runNumber+ " for scenario: "+scenarios);
+        synchronized (VerifyThatTestAreRunOnlyOnceTestOneImpl.class) {
+            assertTrue(runNumber+" / "+VerifyThatTestAreRunOnlyOnceTestOneImpl.scenarios().size()+ " currentScenario: "+scenarios, VerifyThatTestAreRunOnlyOnceTestOneImpl.scenarios().size() >= suiteObject.get());
+        }
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.out.println("After all..");
+    }
+
+
+}
